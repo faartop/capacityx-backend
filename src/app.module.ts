2a3c54465e0cdc7ae268e564910857b17b6ba8e5
Arthur <arthurbrietzke3@gmail.com>
2025-05-28 18:23:02 -0300

    adição de novos modelos para utilização do backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlocacaoModule } from './alocacao/alocacao.module';
 import { ClienteModule } from './cliente/cliente.module';
+import { CategoriaModule } from './categoria/categoria.module';
+import { CategoriaPaiModule } from './categoriapai/categoriapai.module';
+import { ContratoModule } from './contrato/contrato.module';
+import { ItemProjetoCategoriaModule } from './itemprojetocategoria/itemprojetocategoria.module';
+import { ProjetoModule } from './projeto/projeto.module';
+import { ProjetoCategoriaModule } from './projetocategoria/projetocategoria.module';
+import { TecnicoModule } from './tecnico/tecnico.module';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [ClienteModule],
+  imports: [
+    AlocacaoModule,
+    CategoriaModule,
+    CategoriaPaiModule,
+    ClienteModule,
+    ContratoModule,
+    ItemProjetoCategoriaModule,
+    ProjetoModule,
+    ProjetoCategoriaModule,
+    TecnicoModule,
+    UsuarioModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
